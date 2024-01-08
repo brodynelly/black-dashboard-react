7561bb16da53723c3c3d64ab882b5e51ca99958f
brodynelly <127979848+brodynelly@users.noreply.github.com>
2024-01-08 17:22:11 -0600

    Cleaned up code
    deleted some unnecessary files from the theme library

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,139 +1,116 @@
-/*!
 
-=========================================================
-* Black Dashboard React v1.2.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React, {useState} from "react";
+import React, { useState } from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // react plugin used to create charts
 import { Line, Bar } from "react-chartjs-2";
-import Translator from './Translator';
+import Translator from "./Translator";
 
 // reactstrap components
 import {
-  Button,
-  ButtonGroup,
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  Label,
-  FormGroup,
-  Input,
-  Table,
-  Row,
-  Col,
-  UncontrolledTooltip,
+	Button,
+	ButtonGroup,
+	Card,
+	CardHeader,
+	CardBody,
+	CardTitle,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem,
+	UncontrolledDropdown,
+	Label,
+	FormGroup,
+	Input,
+	Table,
+	Row,
+	Col,
+	UncontrolledTooltip,
 } from "reactstrap";
 
 // core components
 import {
-  chartExample1,
-  chartExample2,
-  chartExample3,
-  chartExample4,
+	chartExample1,
+	chartExample2,
+	chartExample3,
+	chartExample4,
 } from "variables/charts.js";
 
 function Dashboard(props) {
-  const [bigChartData, setbigChartData] = React.useState("data1");
-  const setBgChartData = (name) => {
-    setbigChartData(name);
-  };
-  
-  const [screen, setScreen] = useState(1); // Initial screen
-  const [articleInput, setArticleInput] = useState('');
+	const [bigChartData, setbigChartData] = React.useState("data1");
+	const setBgChartData = (name) => {
+		setbigChartData(name);
+	};
 
-  const handleTranslate = () => {
-    setScreen(2); // Move to the next screen after input
-  };
+	const [screen, setScreen] = useState(1); // Initial screen
+	const [articleInput, setArticleInput] = useState("");
 
-  const resetApp = () => {
-    setScreen(1);
-    setArticleInput('');
-  };
-  return (
-    
-    <div className="content">
-      {screen === 1 && (
-        <Col lg="9" md="30">
-        <Card className = "main-text"> 
-        
-        <CardBody>
+	const handleTranslate = () => {
+		setScreen(2); // Move to the next screen after input
+	};
 
-        <CardTitle className="text-center"> 
-              <br/>
-                <b><code  style={{ fontSize: "30px", color: "white" }}>Translatoor</code></b>
-                <code style={{ color: "" }}> paste your article down below to PRACTICE SPANISH!!</code>
-              </CardTitle>
-           
-         
-         
-        <form>
-          <div class="form-group">
-            <Label for="TextToBeTranslated"></Label>
-            <textarea class="form-control" id="TextToBeTranslated" rows="50"
-            value={articleInput} onChange={(e) => setArticleInput(e.target.value)}> 
-              
-            </textarea>
-          </div>
-      
-        </form>
-      <Col  className="ml-auto mr-auto" lg="3">
-        <ButtonGroup
-          className="btn-group-toggle float-none"
-            data-toggle="buttons"
-        >
-        <Button
-          tag="label"
-          className="btn-simple button-outline-gradient"
-          color="green"
-          size="lg"
-          onClick={handleTranslate}
-        >
-           <code 
-           style={{ color: "white" }}>
-            Translate
-          </code>
-        </Button>
-        </ButtonGroup>
-        </Col>
-        </CardBody>
-        </Card>
-        </Col> 
-        
-      )}
+	const resetApp = () => {
+		setScreen(1);
+		setArticleInput("");
+	};
+	return (
+		<div className="content">
+			{screen === 1 && (
+				<Col lg="9" md="30">
+					<Card className="main-text">
+						<CardBody>
+							<CardTitle className="text-center">
+								<br />
+								<b>
+									<code style={{ fontSize: "30px", color: "white" }}>
+										Translatoor
+									</code>
+								</b>
+								<code style={{ color: "" }}>
+									{" "}
+									paste your article down below to PRACTICE SPANISH!!
+								</code>
+							</CardTitle>
 
-      {screen === 2 && (
-        <div>
-          <Translator originalArticle={articleInput} resetApp={resetApp} />
+							<form>
+								<div class="form-group">
+									<Label for="TextToBeTranslated"></Label>
+									<textarea
+										class="form-control"
+										id="TextToBeTranslated"
+										rows="50"
+										value={articleInput}
+										onChange={(e) => setArticleInput(e.target.value)}
+									></textarea>
+								</div>
+							</form>
+							<Col className="ml-auto mr-auto" lg="3">
+								<ButtonGroup
+									className="btn-group-toggle float-none"
+									data-toggle="buttons"
+								>
+									<Button
+										tag="label"
+										className="btn-simple button-outline-gradient"
+										color="green"
+										size="lg"
+										onClick={handleTranslate}
+									>
+										<code style={{ color: "white" }}>Translate</code>
+									</Button>
+								</ButtonGroup>
+							</Col>
+						</CardBody>
+					</Card>
+				</Col>
+			)}
 
-        </div>
-      )}
-    
-        
-
-         
-      
-    </div>
-    
-  );
+			{screen === 2 && (
+				<div>
+					<Translator originalArticle={articleInput} resetApp={resetApp} />
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Dashboard;
